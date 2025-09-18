836ac07df6760ddc33d565add0fc5ee3e2782c86
iZhGH <174579759+iZhGH@users.noreply.github.com>
2025-09-18 14:32:53 -0700

    feat(./frontend): Page header - Import Geist font from Google Fonts - Add SDSU Logo png - Add HTML + CSS for logo, divider and Computer Science text

--- frontend/src/components/SiteHeader.tsx
+++ frontend/src/components/SiteHeader.tsx
@@ -1,8 +1,15 @@
+import sdsuLogo from '../assets/sdsuLogo.png';
 export default function SiteHeader() {
 
 
     return <>
-        <p className="text-white">Header!</p>
+        <div className="flex justify-center">
+            <div className="flex gap-4.5 mt-5">
+            <img src={sdsuLogo} className="h-12 w-auto" alt="SDSU Logo" />
+            <div className="w-[0.1px] h-10 mt-1.5 bg-gray-500"></div>
+            <p className="text-white text-center text-sm mt-1 geist-font">Computer<br></br> Science</p>
+            </div>
+        </div>
     </>
 
 }
