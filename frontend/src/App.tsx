b3ecd4a36381ffaf6431e939f528e484b2bceb19
iZhGH <174579759+iZhGH@users.noreply.github.com>
2025-09-21 23:18:20 -0700

    feat(./frontend): Sign up components - Create SignUpContainer component - Create SignUp feature folder to store respective components - Style SignUpContainer header text, border and background color

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import './App.css'
 import SiteHeader from './components/SiteHeader'
+import SignUpContainer from './features/SignUp/SignUpContainer'
 
 function App() {
 
 
   return (
-      <div className="bg-black min-h-screen">
+      <div className="bg-black min-h-screen flex flex-col items-center gap-10">
         <SiteHeader />
+        <SignUpContainer />
       </div>
   )
 }
---
+++ frontend/src/features/SignUp/SignUpContainer.tsx
@@ -0,0 +1,29 @@
+export default function SignUpContainer() {
+
+    return <>
+        <div className="
+            bg-[#0A0A0A] 
+            w-[500px] h-[631px] 
+            border border-[#282828] rounded-[15px]
+            flex flex-col items-center gap-10
+            p-5">
+
+            <div>
+                <p className="text-white geist-font text-[32px] font-[450]">Sign Up</p>
+            </div>
+
+            <div className="">
+
+
+
+            </div>
+
+
+
+
+        </div>
+
+    </>
+
+
+}
