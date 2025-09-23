c3c003cea1ff6048b7486a0961d97df414f845ec
iZhGH <174579759+iZhGH@users.noreply.github.com>
2025-09-23 16:54:46 -0700

    style(SignUpContainer.tsx): Form styling - Add correct font, positioning and size for input fields - Add input field icons - Add correct input field focus attributes

--- frontend/src/features/SignUp/SignUpContainer.tsx
+++ frontend/src/features/SignUp/SignUpContainer.tsx
@@ -1,3 +1,10 @@
+import { MdOutlineMailOutline } from "react-icons/md";
+import { IoPersonOutline } from "react-icons/io5";
+import { TbLockPassword } from "react-icons/tb";
+
+
+
+
 export default function SignUpContainer() {
 
     return <>
@@ -12,26 +19,25 @@ export default function SignUpContainer() {
                 <p className="text-white geist-font text-[27px] font-[415]">Sign Up</p>
             </div>
 
-            <div className="">
-                <form>
-                    <div>
-                    <label htmlFor="firstName-input" className="text-white">First Name</label>
-                    <input id="firstName-input" type="text" className="bg-white" />
+            <div className="w-full pl-4 pr-4">
+                <form className="flex flex-col gap-3">
+                    <div className="relative">
+                        <label htmlFor="fullName-input" className="text-[#8e8e8e] geist-font text-sm font-[450]">Full Name</label>
+                        <input id="fullName-input" type="text" className="peer text-white text-sm pl-10 pr-2 h-9 w-full bg-neutral-950 border-[#282828] border-[1.5px] rounded-lg focus:border-[rgba(174,58,58,0.4)] focus:outline-none" />
+                        <IoPersonOutline className="absolute left-[10px] top-[72%] -translate-y-1/2 text-[#8e8e8e] peer-focus:text-white text-sm pointer-events-none" />
                     </div>
 
-                    <div>
-                    <label htmlFor="lastName-input" className="text-white">Last Name</label>
-                    <input id="lastName-input" type="text" className="bg-white" />
+
+                    <div className="relative">
+                        <label htmlFor="email-input" className="text-[#8e8e8e] geist-font text-sm font-[450]">Email</label>
+                        <input id="email-input" type="email" className="peer text-white text-sm pl-10 pr-2 h-9 w-full bg-neutral-950 border-[#282828] border-[1.5px] rounded-lg focus:border-[rgba(174,58,58,0.4)] focus:outline-none" />
+                        <MdOutlineMailOutline className="absolute left-[10px] top-[72%] transform -translate-y-1/2 text-[#8e8e8e] peer-focus:text-white text-sm pointer-events-none" />
                     </div>
 
-                    <div>
-                    <label htmlFor="email-input" className="text-white">Email</label>
-                    <input id="email-input" type="email" className="bg-white" />
-                    </div>
-
-                    <div>
-                    <label htmlFor="password-input" className="text-white">Password</label>
-                    <input id="password-input" type="password" className="bg-white" />
+                    <div className="relative">
+                        <label htmlFor="password-input" className="text-[#8e8e8e] geist-font text-sm font-[450]">Password</label>
+                        <input id="password-input" type="password" className="peer text-white text-sm pl-10 pr-2 h-9 w-full bg-neutral-950 border-[#282828] border-[1.5px] rounded-lg focus:border-[rgba(174,58,58,0.4)] focus:outline-none" />
+                        <TbLockPassword className="absolute left-[10px] top-[70%] -translate-y-1/2 text-[#8e8e8e] peer-focus:text-white text-sm pointer-events-none" />
                     </div>
 
                 </form>
