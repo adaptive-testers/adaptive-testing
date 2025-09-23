7dfc343e17a00996ba00a0bb0c465a466f892e2b
iZhGH <174579759+iZhGH@users.noreply.github.com>
2025-09-23 16:03:49 -0700

    feat(SignUpContainer.tsx): Sign-up form - Add basic HTML + CSS for sign up inputs

--- frontend/src/features/SignUp/SignUpContainer.tsx
+++ frontend/src/features/SignUp/SignUpContainer.tsx
@@ -7,13 +7,34 @@ export default function SignUpContainer() {
             border-[1.5px] border-[#282828] rounded-[15px] 
             flex flex-col items-center gap-10
             p-5">
-
+                    
             <div>
                 <p className="text-white geist-font text-[27px] font-[415]">Sign Up</p>
             </div>
 
             <div className="">
+                <form>
+                    <div>
+                    <label htmlFor="firstName-input" className="text-white">First Name</label>
+                    <input id="firstName-input" type="text" className="bg-white" />
+                    </div>
 
+                    <div>
+                    <label htmlFor="lastName-input" className="text-white">Last Name</label>
+                    <input id="lastName-input" type="text" className="bg-white" />
+                    </div>
+
+                    <div>
+                    <label htmlFor="email-input" className="text-white">Email</label>
+                    <input id="email-input" type="email" className="bg-white" />
+                    </div>
+
+                    <div>
+                    <label htmlFor="password-input" className="text-white">Password</label>
+                    <input id="password-input" type="password" className="bg-white" />
+                    </div>
+
+                </form>
 
 
             </div>
