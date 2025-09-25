7993f963de69313a094498c1c7f43978e9a07dc3
iZhGH <174579759+iZhGH@users.noreply.github.com>
2025-09-24 18:22:40 -0700

    fix(App.tsx): Component spacing - Reduce gap between header and site content

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,7 +7,7 @@ function App() {
 
 
   return (
-      <div className="bg-black min-h-screen flex flex-col items-center gap-15">
+      <div className="bg-black min-h-screen flex flex-col items-center gap-10">
         <SiteHeader />
         <SignUpContainer />
       </div>
