5b8f65850ab737ee94cd0ee2cc9f0a4efd004cb8
iZhGH <174579759+iZhGH@users.noreply.github.com>
2025-09-25 11:32:39 -0700

    style(SignUpContainer.tsx): Specific required messages - Add specific required messages for each input upon error

--- frontend/src/features/SignUp/SignUpContainer.tsx
+++ frontend/src/features/SignUp/SignUpContainer.tsx
@@ -45,7 +45,7 @@ export default function SignUpContainer() {
                 <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-3">
                     <div className="relative">
                         <label htmlFor="fullName-input" className="text-[#8e8e8e] geist-font text-sm font-[375]">Full Name</label>
-                        <input {...register("fullName", { required: "This is required" })} id="fullName-input" type="text" className="peer text-white text-sm pl-10 pr-2 h-9 w-full bg-neutral-950 border-[#282828] border-[2px] rounded-lg focus:border-[rgba(174,58,58,0.4)] focus:outline-none" />
+                        <input {...register("fullName", { required: "Name is required" })} id="fullName-input" type="text" className="peer text-white text-sm pl-10 pr-2 h-9 w-full bg-neutral-950 border-[#282828] border-[2px] rounded-lg focus:border-[rgba(174,58,58,0.4)] focus:outline-none" />
                         <IoPersonOutline className="absolute left-[10px] top-[72%] -translate-y-1/2 text-[#8e8e8e] peer-focus:text-white text-sm pointer-events-none" />
                        {errors.fullName && (
                         <p className="absolute left-0 bottom-[-18px] geist-font text-red-500 text-[10px]">
@@ -57,7 +57,7 @@ export default function SignUpContainer() {
 
                     <div className="relative">
                         <label htmlFor="email-input" className="text-[#8e8e8e] geist-font text-sm font-[375]">Email</label>
-                        <input {...register("userEmail", { required: "This is required" })} id="email-input" type="email" className="peer text-white text-sm pl-10 pr-2 h-9 w-full bg-neutral-950 border-[#282828] border-[2px] rounded-lg focus:border-[rgba(174,58,58,0.4)] focus:outline-none" />
+                        <input {...register("userEmail", { required: "Email is required" })} id="email-input" type="email" className="peer text-white text-sm pl-10 pr-2 h-9 w-full bg-neutral-950 border-[#282828] border-[2px] rounded-lg focus:border-[rgba(174,58,58,0.4)] focus:outline-none" />
                         <MdOutlineMailOutline className="absolute left-[10px] top-[72%] transform -translate-y-1/2 text-[#8e8e8e] peer-focus:text-white text-sm pointer-events-none" />
                         {errors.userEmail && (
                         <p className="absolute left-0 bottom-[-18px] geist-font text-red-500 text-[10px]">
@@ -71,7 +71,7 @@ export default function SignUpContainer() {
                         <input
                             id="password-input"
                             type={showPassword ? "text" : "password"}
-                            {...register("userPassword", { required: "This is required", minLength: { value: 8, message: "Minimum length is 8" } })}
+                            {...register("userPassword", { required: "Password is required", minLength: { value: 8, message: "Minimum length is 8" } })}
                             className="peer text-white text-sm pl-10 pr-10 h-9 w-full bg-neutral-950 border-[#282828] border-[2px] rounded-lg focus:border-[rgba(174,58,58,0.4)] focus:outline-none"
                         />
                         <TbLockPassword className="absolute left-[10px] top-[70%] -translate-y-1/2 text-[#8e8e8e] peer-focus:text-white text-sm pointer-events-none" />
