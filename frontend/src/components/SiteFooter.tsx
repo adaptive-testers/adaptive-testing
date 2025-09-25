78c7a12b80635ca494a0288b18297ea1fdc0b651
iZhGH <174579759+iZhGH@users.noreply.github.com>
2025-09-25 10:40:49 -0700

    fix(SiteFooter.tsx): Grammar - Add comma inbetween footer sentence

--- frontend/src/components/SiteFooter.tsx
+++ frontend/src/components/SiteFooter.tsx
@@ -2,7 +2,7 @@ export default function SiteFooter() {
     return <>
         <div className="flex justify-center gap-1 mt-15">
                     <p className="text-[#C50A2C] geist-font text-[14px] font-light">Disclaimer: </p>
-                    <p className="text-white geist-font font-light text-[14px]">This site is NOT officially affiliated with San Diego State University it is a senior project for CS496 2025-26</p>
+                    <p className="text-white geist-font font-light text-[14px]">This site is NOT officially affiliated with San Diego State University, it is a senior project for CS496 2025-26</p>
                         
         </div>
         </>
