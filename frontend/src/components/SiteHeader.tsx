cab420d7d9abfae60666b9508cb5352fd61c82cb
iZhGH <174579759+iZhGH@users.noreply.github.com>
2025-09-24 18:42:39 -0700

    style(SiteHeader.tsx): Header button - Wrap header into button - Add scale hover effect to header - Add cursor pointer to header

--- frontend/src/components/SiteHeader.tsx
+++ frontend/src/components/SiteHeader.tsx
@@ -3,13 +3,17 @@ export default function SiteHeader() {
 
 
     return <>
+
         <div className="flex justify-center">
+            <button className="transform   origin-center will-change-transform transition-transform hover:scale-110 cursor-pointer">
             <div className="flex gap-4.5 mt-5">
             <img src={sdsuLogo} className="h-12 w-auto" alt="SDSU Logo" />
             <div className="w-[0.1px] h-10 mt-1.5 bg-gray-500"></div>
             <p className="text-white text-center text-sm mt-1 geist-font">Computer<br></br> Science</p>
             </div>
+            </button>
         </div>
+       
     </>
 
 }
