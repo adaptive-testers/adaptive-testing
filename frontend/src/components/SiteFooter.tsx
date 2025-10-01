91b8877d342f01a7557eeb51144a9ed9b85188d0
iZhGH <174579759+iZhGH@users.noreply.github.com>
2025-10-01 13:16:25 -0700

    style(SiteFooter.tsx): Footer visibility - Make footer stand out less by changing font size and color

--- frontend/src/components/SiteFooter.tsx
+++ frontend/src/components/SiteFooter.tsx
@@ -1,8 +1,8 @@
 export default function SiteFooter() {
     return <>
         <div className="flex justify-center gap-1 mt-15">
-                    <p className="text-[#C50A2C] geist-font text-[14px] font-light">Disclaimer: </p>
-                    <p className="text-white geist-font font-light text-[14px]">This site is NOT officially affiliated with San Diego State University, it is a senior project for CS496 2025-26</p>
+                    <p className="text-gray-500 geist-font text-[12px] font-medium">Disclaimer: </p>
+                    <p className="text-gray-500 geist-font font-medium text-[12px]">This site is <span className="text-[#C50A2C]">NOT</span> officially affiliated with San Diego State University, it is a senior project for CS496 2025-26</p>
                         
         </div>
         </>
