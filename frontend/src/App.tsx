92b98107951f91d82c350a5edca46ecbe10a99a9
iZhGH <174579759+iZhGH@users.noreply.github.com>
2025-10-03 13:40:44 -0700

    feat(./frontend) Sign Up Page Testing - Add iniital tests to confirm sign up page renders correct form labels - Add initial test to confirm create acocunt button exists

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import SignUpPage from './features/SignUp/SignUpPage'
-
+import { AuthProvider } from './context/AuthContext'
 
 function App() {
   return (
+     <AuthProvider>
       <div className="bg-black min-h-screen flex flex-col items-center justify-center gap-10">
         <SignUpPage />
       </div>
+    </AuthProvider>
   )
 }
 
