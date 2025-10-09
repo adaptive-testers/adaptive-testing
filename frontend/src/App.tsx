1b31dfc7c74d5a06b5b0885b3e0f77d5e9a7360f
JamesDuong7 <duongjames7@gmail.com>
2025-10-08 18:31:20 -0700

    feat(./frontend) Renamed LogInContainer with LogInPage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import { AuthProvider } from "./context/AuthContext";
-import LogInContainer from "./features/LogIn/LogInContainer";
+import LogInPage from "./features/LogIn/LogInPage";
 import SignUpPage from "./features/SignUp/SignUpPage";
 
 function App() {
@@ -10,9 +10,9 @@ function App() {
       <AuthProvider>
         <div className="bg-black min-h-screen flex flex-col items-center justify-center gap-10">
           <Routes>
-            <Route path="/" element={<LogInContainer />} />
+            <Route path="/" element={<LogInPage />} />
             <Route path="/signup" element={<SignUpPage />} />
-            <Route path="/login" element={<LogInContainer />} />
+            <Route path="/login" element={<LogInPage />} />
           </Routes>
         </div>
       </AuthProvider>
