14bfadc5deb23c5c53e6154b6a37783c3de1d80a
JamesDuong7 <duongjames7@gmail.com>
2025-10-08 18:24:39 -0700

    feat: Implement routing and login functionality with react-router-dom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,23 @@
-import { AuthProvider } from './context/AuthContext'
-import SignUpPage from './features/SignUp/SignUpPage'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import { AuthProvider } from "./context/AuthContext";
+import LogInContainer from "./features/LogIn/LogInContainer";
+import SignUpPage from "./features/SignUp/SignUpPage";
 
 function App() {
   return (
-     <AuthProvider>
-      <div className="bg-black min-h-screen flex flex-col items-center justify-center gap-10">
-        <SignUpPage />
-      </div>
-    </AuthProvider>
-  )
+    <BrowserRouter>
+      <AuthProvider>
+        <div className="bg-black min-h-screen flex flex-col items-center justify-center gap-10">
+          <Routes>
+            <Route path="/" element={<LogInContainer />} />
+            <Route path="/signup" element={<SignUpPage />} />
+            <Route path="/login" element={<LogInContainer />} />
+          </Routes>
+        </div>
+      </AuthProvider>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
