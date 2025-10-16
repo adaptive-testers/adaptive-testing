ec2f4822004a1250f76143f644a1a6f408e8a911
JamesDuong7 <155055896+JamesDuong7@users.noreply.github.com>
2025-10-16 16:57:59 -0700

    feat(frontend)/login-page (#66)
    * feat(./frontend): Log In UI
    
    * feat: Implement routing and login functionality with react-router-dom
    
    * feat(./frontend) Renamed LogInContainer with LogInPage
    
    * feat(./frontend) Update @testing-library/jest-dom to version 6.9.1 and add LogInPage tests
    
    * fix(LogInPage): remove redundant show password label and improve accessibility for keep signed in button
    
    * build(deps): Update frontend dependencies to latest versions
    
    - Bumped various frontend dependencies including:
      - @eslint/js to 9.37.0
      - @types/react to 19.2.2
      - @types/react-dom to 19.2.1
      - eslint to 9.37.0
      - eslint-plugin-react-refresh to 0.4.23
      - jiti to 2.6.1
      - typescript to 5.9.3
      - typescript-eslint to 8.46.0
      - vite to 7.1.9
      - react to 19.2.0
      - react-dom to 19.2.0
      - tailwindcss to 4.1.14
      - @vitejs/plugin-react to 5.0.4
      - @testing-library/jest-dom to 6.9.1
      - react-hook-form to 7.65.0
      - react-router and react-router-dom to 7.9.4
      - rollup to 4.52.4
      - zod to 4.1.12
      - zod-validation-error to 4.0.2
    
    This update includes minor and patch version upgrades to improve performance and security.
    
    ---------
    
    Co-authored-by: talberry <52144102+talberry@users.noreply.github.com>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,23 @@
-import SignUpPage from './features/SignUp/SignUpPage'
-import './index.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { AuthProvider } from "./context/AuthContext";
+import LogInPage from "./features/LogIn/LogInPage";
+import SignUpPage from "./features/SignUp/SignUpPage";
 
 function App() {
   return (
-      <div className="bg-black min-h-screen flex flex-col items-center justify-center gap-10">
-        <SignUpPage />
-      </div>
-  )
+    <BrowserRouter>
+      <AuthProvider>
+        <div className="bg-black min-h-screen flex flex-col items-center justify-center gap-10">
+          <Routes>
+            <Route path="/" element={<LogInPage />} />
+            <Route path="/signup" element={<SignUpPage />} />
+            <Route path="/login" element={<LogInPage />} />
+          </Routes>
+        </div>
+      </AuthProvider>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
