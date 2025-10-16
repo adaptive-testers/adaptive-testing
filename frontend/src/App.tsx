35c63bcdef8872d57004dcc0dd9ac79f4e3085d3
iZhGH <174579759+iZhGH@users.noreply.github.com>
2025-10-15 20:41:36 -0700

    PR fixes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,11 @@
-import { AuthProvider } from './context/AuthContext'
 import SignUpPage from './features/SignUp/SignUpPage'
 import './index.css'
 
 function App() {
   return (
-     <AuthProvider>
       <div className="bg-black min-h-screen flex flex-col items-center justify-center gap-10">
         <SignUpPage />
       </div>
-    </AuthProvider>
   )
 }
 
